9e031a256f2d5f5120672b7828dfe5b912319122
davipythonweb <galaxys3tab32@gmail.com>
2024-09-03 08:28:31 -0300

    create exemple

---
+++ exemple.py
@@ -0,0 +1,13 @@
+def minhaFuncao():
+    """ descricao da funcao"""
+    print('Hello World')
+
+def essa_eh_uma_funcao():
+   
+    """ descrição da funçao"""
+    print('Ola Mundo')
+
+
+# essa_eh_minha_funcao -> snake_case === variaveis e funçoes
+# minhaFuncao -> cammelCase === NAõ utilizar em codigo
+# MinhaFuncao -> PascalCase === classes
